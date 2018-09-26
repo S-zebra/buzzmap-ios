f67e433157522526d9cd61e86ad19f9dbf239168
S-zebra <shimashimazebra13@gmail.com>
2018-09-26 09:26:39 +0900

    Initial Commit

---
+++ buzzmap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  buzzmap
+//
+//  Created by kazu on 9/26/18.
+//  Copyright © 2018 uemtp2018. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
